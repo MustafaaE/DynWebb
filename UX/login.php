93bbefe5d12f04a093b4b9b3dd242e78039db278
LAPTOP-UE4NHFS6\armin <armindjulovic@msn.com>
2022-01-06 14:50:54 +0100

    Co-authored-by: Tova0811 <Tova0811@users.noreply.github.com>

---
+++ UX/login.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="styles.css">
+  <title>Login Page</title>
+</head>
+<body>
+<div class="wrapper">
+  <div class="container">
+    <div class="col-left">
+      <div class="login-form">
+        <h2>Login</h2>
+        <form>
+          <p>
+            <input type="email" placeholder="Email" required>
+          </p>
+          <p>
+            <input type="password" placeholder="Password" required>
+          </p>
+          <p>
+            <input class="btn" type="submit" value="Sign In" />
+          </p>
+          <p>
+            <a href="">Forget Password?</a>
+          </p>
+          <a href ="register.php"> Create Account</a>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
---
+++ components/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../UX/styles.css">
+    <title>Header</title>
+</head>
+<body>
+
+    <nav class="header-nav">
+        <li><a href="../UX/index.php">Home</a></li>
+        <li><a href="../UX/profil.php">Profile</a></li>
+        <li><a href="../UX/">Upload</a></li>
+        <li><a href="../UX/login.php">Log out</a></li>
+    </nav>
+
+    <p></p>Search</p>
