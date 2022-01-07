fd4efd232ca21b375511240884ddd9ee5aa8df35
Tova0811 <tova.folkesson@gmail.com>
2022-01-07 21:04:37 +0100

    header and search bar

---
+++ components/methods.php
@@ -0,0 +1,16 @@
+<?php 
+session_start();
+
+function redirectTo($url = null)
+{
+    $url = $url ?? '';
+    header("Location: http://localhost/grupparbete_dynweb/DynWebb/$url");
+    exit;
+}
+
+function isUserLoggedIn()
+{
+    if(!isset($_SESSION['user'])){
+        redirectTo('../dynwebb/UX/login.php');
+    }
+}
---
+++ interface/register_user.php
@@ -0,0 +1,28 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require 'connection.php';
+
+$pdo = connectToDB();
+
+
+$email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+$username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
+$password = filter_input(INPUT_POST, 'password',FILTER_SANITIZE_STRING);
+
+$hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+$statement = $pdo ->prepare('insert into users(username,password,email)
+values (:username,:password,:email)');
+$statement ->bindValue('username', $username);
+$statement ->bindValue('password', $hashedPassword );
+$statement ->bindValue('email', $email);
+
+try {
+    $statement->execute();
+    header("Location: ../UX/index.php");
+} catch (PDOException $e) {
+    var_dump($e ->getMessage());
+}
